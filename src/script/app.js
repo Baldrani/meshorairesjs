941d3862f6885e994e91d2777b33c7e755642b1f
Baldrani <mael.mayon@free.fr>
2019-01-11 17:35:24 +0100

    SWANG

--- src/script/app.js
+++ src/script/app.js
@@ -1,22 +1,3 @@
 /* Récupération du module direct depuis `export default function ()` */
 import tools from "./components/navigator.js";
-/* Récupération des exports nommés du module avec `export function <name>()` */
-import { addition, substraction, multiplication, division } from "./components/navigator.js";
-/* Variables à tester. */
-var number1 = 13,
-    number2 = 7.7;
-/* Utilisation des fonctions de nos modules. */
-console.log('addition', addition(number1, number2));
-console.log('substraction', substraction(number1, number2));
-console.log('multiplication', multiplication(number1, number2));
-console.log('division', division(number1, number2));
-console.log('round', tools(number2).round);
-console.log('floor', tools(number2).floor);
-console.log('ceil', tools(number2).ceil);
-
-//div = document.createElement('div')
-//div.innerHTML = "Test"
-
-//const app = document.getElementById('app')
-
-//document.getElementById('app').append(div)
+ tools()
--- src/script/components/navigator.js
+++ src/script/components/navigator.js
@@ -1,24 +1,4 @@
 /* Export direct. */
-export default function (number) {
-    return {
-        round: Math.round(number),
-        floor: Math.floor(number),
-        ceil: Math.ceil(number)
-    };
-}
-/* Export nommé `addition`. */
-export function addition(number1, number2) {
-    return number1 + number2;
-}
-/* Export nommé `substraction`. */
-export function substraction(number1, number2) {
-    return number1 - number2;
-}
-/* Export nommé `multiplication`. */
-export function multiplication(number1, number2) {
-    return number1 * number2;
-}
-/* Export nommé `division`. */
-export function division(number1, number2) {
-    return number1 / number2;
+export default function () {
+    return console.log('test')
 }
