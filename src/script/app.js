7bad34a1af8517736060191c9dbca736ec789b4a
Robilol <pimous78920@gmail.com>
2019-01-15 23:31:11 +0100

    form de l'angoisse + webpack c dur

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,5 @@
 /* Récupération du module direct depuis `export default function ()` */
 import tools from "./components/navigator.js";
+import form from "./components/form.js";
  tools()
+form()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-    entry: './src/script/app.js',
+    entry: {
+        js: './src/script/app.js',
+        sass: './src/style/style.sass',
+    },
     output: {
         path: __dirname + "/public",
         filename: "app.js"
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new BrowserSyncPlugin({
@@ -13,6 +30,25 @@ module.exports = {
             host: 'localhost',
             port: 3000,
             server: { baseDir: ['public'] }
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    }
 }
